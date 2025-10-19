136a40cf31330576beda89b32996d6b7490e4dff
judy <judyhenshaw01@gmail.com>
2025-10-19 01:00:43 +0100

    Fix: Disable debug mode

--- app.py
+++ app.py
@@ -77,4 +77,4 @@ def ratelimit_handler(e):
 if __name__ == '__main__':
     logger.info('Starting Flask application')
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=False, host='0.0.0.0', port=port)
